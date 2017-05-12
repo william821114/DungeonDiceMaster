3d6fd9c66bba70e601ba8099d3fc73412239f04a
WilliamKe <william_ke@WilliamKede-MacBook-Pro.local>
2017-05-12 20:23:09 +0800

    Prototype 2 - Select Battle Skill (60%)

---
+++ Assets/Scripts/new/SkillButtonGestureManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TouchScript.Gestures;
+
+public class SkillButtonGestureManager : MonoBehaviour {
+
+	public TapGesture singleTap;
+	public LongPressGesture longPress;
+
+	public Sprite skillON;
+	public Sprite skillOff;
+
+	private bool state = false;
+	private SpriteRenderer icon;
+
+	// Use this for initialization
+	void Start () {
+
+		icon = this.gameObject.GetComponent<SpriteRenderer> ();
+
+		singleTap.Tapped += (object sender, System.EventArgs e) => 
+		{
+			if(state){
+				icon.sprite = skillOff;
+				state = false;
+			} else{
+				icon.sprite = skillON;
+				state = true;
+			}
+		};
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
