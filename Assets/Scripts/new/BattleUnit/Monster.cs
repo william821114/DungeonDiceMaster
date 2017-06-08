6fdb3da81a4f87fd6d30f7a4329e1b38022b8a31
ballchen <kerkerball@gmail.com>
2017-06-08 20:15:50 +0800

    Design SkillEffect for Monster 20%

--- Assets/Scripts/new/BattleUnit/Monster.cs
+++ Assets/Scripts/new/BattleUnit/Monster.cs
@@ -5,7 +5,8 @@ using DiceMaster;
 
 
 // 目前還缺受傷的動畫、攻擊的動畫
-public class Monster : BattleUnit {
+public class Monster : BattleUnit
+{
 
     public int MaxHp;
     public int MaxMp;
@@ -14,15 +15,15 @@ public class Monster : BattleUnit {
 
     public int Hp;
     public int Mp;
-	public int Atk;
-	public int Def;
+    public int Atk;
+    public int Def;
 
-	public Dice[] battleDice;
-	public Dice exploreDice;
+    public Dice[] battleDice;
+    public Dice exploreDice;
     public bool willGetHurt = false;
 
-	private bool isDead = false;
-	private Animator monsterAnimator;
+    private bool isDead = false;
+    private Animator monsterAnimator;
 
     public Character actioningCharacter;
     public StateManager stateManager;
@@ -30,9 +31,10 @@ public class Monster : BattleUnit {
     //public TextMesh monsterHurtValueText;
 
 
-	void Start () {
-		stateManager = (StateManager)FindObjectOfType (typeof(StateManager));
-	}
+    void Start()
+    {
+        stateManager = (StateManager)FindObjectOfType(typeof(StateManager));
+    }
     /*
     void Start(){
 		monsterAnimator = this.GetComponent<Animator> ();
@@ -69,7 +71,7 @@ public class Monster : BattleUnit {
     public void check(int finalCheckValue)
     {
         int damage = finalCheckValue - this.Def;
-        if(damage > 0)
+        if (damage > 0)
         {
             willGetHurt = true;
             Debug.Log("Monster: get Hurt - " + damage);
@@ -81,34 +83,78 @@ public class Monster : BattleUnit {
 
             this.Hp -= damage;
 
-            if (this.Hp < 0) this.Hp = 0;
+            if (this.Hp <= 0) this.Hp = 0;
             //monsterHurtValueText.text = "-" + damage;
         }
     }
 
     public void AI(int[] checkValue)
     {
-        //先做一個最簡單的ai，隨機選一個人，直接把finalcheckvalue當傷害打出去
+        // 怪物血量大於50%時，若骰子總和大於 6 ，攻擊加 5
+        // 怪物血量小於50 %時，50%機率 : 補血補到滿
+        // 50% 機率 發動困獸之鬥: 如果所有骰子都大於2，攻擊最後 *2
         int finalCheckValue = 0;
+        int k = Random.Range(0, 15000);
+        Character target = stateManager.getCharacter();
 
-        for (int i = 0; i < checkValue.Length; i++)
+        if (this.Hp > this.MaxHp / 2)
         {
-            finalCheckValue += checkValue[i];
+            for (int i = 0; i < checkValue.Length; i++)
+            {
+                finalCheckValue += checkValue[i];
+            }
+
+            if (finalCheckValue >= 6)
+            {
+                finalCheckValue += 5;
+                Debug.Log("猛獸踢發動成功! -" + finalCheckValue);
+            }
         }
-			
-		Character target = stateManager.getCharacter();
+
+        else
+        {
+            if (k < 7500)
+            {
+                // heal
+                Debug.Log("Monster Heal");
+            }
+
+            else
+            {
+                bool isAllBiggerthan2 = true;
+                for (int i = 0; i < checkValue.Length; i++)
+                {
+                    if (finalCheckValue < 2)
+                    {
+                        isAllBiggerthan2 = false;
+                    }
+                    finalCheckValue += checkValue[i];
+                }
+
+                if (isAllBiggerthan2)
+                {
+                    finalCheckValue *= 2;
+                    Debug.Log("困獸之鬥發動成功! -" + finalCheckValue);
+                }
+            }
+        }
+
         target.check(finalCheckValue);
+
+
     }
 
-	public Dice[] getBattleDice(){
-		return battleDice;
-	}
+    public Dice[] getBattleDice()
+    {
+        return battleDice;
+    }
 
-	public Dice getExploreDice(){
-		return exploreDice;
-	}
+    public Dice getExploreDice()
+    {
+        return exploreDice;
+    }
 
-	/*
+    /*
 	// 播死亡的fade out動畫
 	private void PlayDieAnimation(){
 		monsterAnimator.SetTrigger ("Die");
---
+++ Assets/Scripts/new/Skills/MonsterSkillEffect.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSkillEffect
+{
+    public bool isDamage;
+    public bool isHeal;
+    public bool isMPDamage;
+    public bool isHealMP;
+    public bool isDodge;
+    public bool isShield;
+    public bool isDisable;
+    public bool isSelfDisable;
+    public bool isSkillActivated;
+    public int damage;
+    public int heal;
+    public int MPDamage;
+    public int healMP;
+    public int dodge;
+    public float shield;
+    public int disable;
+    public int disableTurns;
+    public int selfDisable;
+    public int selfDisableTurns;
+
+    public MonsterSkillEffect()
+    {
+        this.isDamage = false;
+        this.isHeal = false;
+        this.isDodge = false;
+        this.isShield = false;
+        this.isDisable = false;
+        this.isSelfDisable = false;
+        this.isMPDamage = false;
+        this.isHealMP = false;
+
+        // 初始值設定 activated 為 false，在 Skill 中的 calSkillEffect 發動成功時，
+        // 將這個參數設定為 true ，以觸發技能發動的特效
+        this.isSkillActivated = false;
+    }
+
+    public void setHeal(int val)
+    {
+        this.heal = val;
+    }
+
+    public void setDamage(int val)
+    {
+        this.damage = val;
+    }
+
+    public void setDodge(int val)
+    {
+        this.dodge = val;
+    }
+
+    public void setShield(float val)
+    {
+        this.shield = val;
+    }
+
+    public void setDisable(int val, int turn)
+    {
+        this.disable = val;
+        this.disableTurns = turn;
+    }
+
+    public void setSelfDisable(int val, int turn)
+    {
+        this.selfDisable = val;
+        this.selfDisableTurns = turn;
+    }
+
+    public void setMPDamage(int val)
+    {
+        this.MPDamage = val;
+    }
+
+    public void setHealMP(int val)
+    {
+        this.healMP = val;
+    }
+
+    public void setSkillActivated(bool val)
+    {
+        this.isSkillActivated = val;
+    }
+
+}
